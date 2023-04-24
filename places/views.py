4af2a424351135b1d1b6ea006e68c07d8099bbd9
Polina <75391420+wezbicka@users.noreply.github.com>
2023-04-24 15:44:52 +0300

    Add blank start page

--- places/views.py
+++ places/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+
 
 def show_main_page(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    return render(request, 'index.html')
