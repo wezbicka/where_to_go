814600b2d56db98964e31f60917a50eba3f7952e
Polina <75391420+wezbicka@users.noreply.github.com>
2023-05-23 19:44:17 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
 
 admin.site.register(Place)
+admin.site.register(Image)
--- places/models.py
+++ places/models.py
@@ -9,3 +9,22 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Image(models.Model):
+    order_number = models.PositiveIntegerField(
+        'Порядковый номер',
+        default=0,
+        db_index=True,
+    )
+    place = models.ForeignKey(
+        Place,
+        verbose_name='Место',
+        related_name='images',
+        on_delete=models.CASCADE
+    )
+    file = models.FileField("Картинка", upload_to="media")
+
+    def __str__(self):
+        return f"{self.order_number} {self.place.title}"
+    
