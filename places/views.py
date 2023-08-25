2afdafffdbf070b118cfb5b6b64dabbfe2f24f38
Polina <75391420+wezbicka@users.noreply.github.com>
2023-08-25 10:29:20 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -2,4 +2,35 @@ from django.shortcuts import render
 
 
 def show_main_page(request):
-    return render(request, 'index.html')
+    context = {
+        "places": {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [37.62, 55.793676]
+                    },
+                    "properties": {
+                        "title": "«Легенды Москвы",
+                        "placeId": "moscow_legends",
+                        "detailsUrl": "static/places/moscow_legends.json"
+                    }
+                },
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [37.64, 55.753676]
+                    },
+                    "properties": {
+                        "title": "Крыши24.рф",
+                        "placeId": "roofs24",
+                        "detailsUrl": "static/places/roofs24.json"
+                    }
+                }
+            ]
+        }
+    }
+    return render(request, 'index.html', context)
