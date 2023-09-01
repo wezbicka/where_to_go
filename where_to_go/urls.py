50a11220329b45997c09959d8a318f8af7947f5c
Polina <75391420+wezbicka@users.noreply.github.com>
2023-09-01 13:54:34 +0300

    Add WYSIWYG editor in UI

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from places import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,5 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('places/<int:place_id>/', views.place_detail,  name='place-detail')
+    path('places/<int:place_id>/',
+        views.place_detail, 
+        name='place-detail'),
+    path('tinymce/', include('tinymce.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
