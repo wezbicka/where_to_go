60f57d337fc41e745417bc99a77ccb5f2b26901e
Polina <75391420+wezbicka@users.noreply.github.com>
2023-09-04 14:19:36 +0300

    Delete STATICFILES_DIRS

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -124,10 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_URL = '/static/'
 
 # Default primary key field type
