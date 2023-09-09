a4d3673d170f563fab059625b3555d6be4be0b5e
Polina <75391420+wezbicka@users.noreply.github.com>
2023-09-09 10:24:14 +0300

    Update settings,py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -134,11 +134,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
