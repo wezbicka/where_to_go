aa28d0b039f5d74491f058f75b5c976306750d78
Polina <75391420+wezbicka@users.noreply.github.com>
2023-09-09 17:53:50 +0300

    Update urls.py according to PEP8

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -24,8 +24,10 @@ from places import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('places/<int:place_id>/',
-        views.place_detail, 
-        name='place-detail'),
+    path(
+        'places/<int:place_id>/',
+        views.place_detail,
+        name='place-detail'
+    ),
     path('tinymce/', include('tinymce.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
